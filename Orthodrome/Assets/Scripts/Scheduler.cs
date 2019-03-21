ba32ad254427232944cef2f5abca6ca4d724865f
marax27 <kacpert314@gmail.com>
2019-03-21 19:59:49 +0100

    Add "Simple Notification" prefab, basic HUD setup, add Scheduler

---
+++ Orthodrome/Assets/Scripts/Scheduler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scheduler : MonoBehaviour {
+
+	public GameObject topArea;
+	public GameObject bottomArea;
+	public GameObject leftArea;
+	public GameObject rightArea;
+	public GameObject centerArea;
+	public GameObject frontArea;
+
+}
