b46f10cee12cde5ae73c326192567d2bc23c97f5
marax27 <kacpert314@gmail.com>
2019-03-21 20:36:26 +0100

    Rename Simple Notification, add Notification.cs, Scheduler.Request()

---
+++ Orthodrome/Assets/Scripts/Notification.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notification : MonoBehaviour {
+
+	public Text titleText;
+	public Text descriptionText;
+
+}
--- Orthodrome/Assets/Scripts/Scheduler.cs
+++ Orthodrome/Assets/Scripts/Scheduler.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class Scheduler : MonoBehaviour {
 
+	[Header("Screen Areas")]
 	public GameObject topArea;
 	public GameObject bottomArea;
 	public GameObject leftArea;
@@ -11,4 +12,23 @@ public class Scheduler : MonoBehaviour {
 	public GameObject centerArea;
 	public GameObject frontArea;
 
+	[Header("Prefabs")]
+	public Notification notificationPrefab;
+
+	//************************************************************
+
+	/// <summary>
+	/// Attempt to display a notification. Return true or false, depending
+	/// whether Scheduler succedeed or not.
+	/// </summary>
+	/// <param name="title">Notification title</param>
+	/// <param name="description">Notification description text</param>
+	/// <returns>Returns true if notification's been displayed succesfully.</returns>
+	public bool Request(string title, string description) {
+		Notification obj = Instantiate(notificationPrefab, leftArea.transform) as Notification;
+		obj.titleText.text = title;
+		obj.descriptionText.text = description;
+
+		return true;
+	}
 }
