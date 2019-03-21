373516b1ecc23249a4c8ccc49fe404d4b95d485b
marax27 <kacpert314@gmail.com>
2019-03-21 22:03:02 +0100

    Add simple Scheduler implementation, add notification-sending object
    NotifSender and Dummy object are only for testing and will soon be deleted.

---
+++ Orthodrome/Assets/Scripts/NotifSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotifSender : MonoBehaviour
+{
+	public Scheduler scheduler;
+	public Text outputText;
+	public GameObject area;
+	public Canvas canvas;
+
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			scheduler.Request(
+				"Title",
+				"If I manually deselect and reselect \"Preferred Height\" in the Layout Element component, the preferredHeight value changes."
+			);
+		}
+
+		var r0 = area.GetComponent<RectTransform>().rect;
+		string s0 = string.Format("rect: {{{0}, <b>{1}</b>}}", r0.width * canvas.scaleFactor, r0.height * canvas.scaleFactor);
+
+		var vlg = area.GetComponent<VerticalLayoutGroup>();
+		string s1 = string.Format("flex: {{{0}, <b>{1}</b>}}", vlg.flexibleWidth, vlg.flexibleHeight);
+		string s2 = string.Format("pref: {{{0}, <b>{1}</b>}}", vlg.preferredWidth, vlg.preferredHeight);
+		string s3 = string.Format("min: {{{0}, <b>{1}</b>}}", vlg.minWidth, vlg.minHeight);
+
+		float freeHeight = r0.height * canvas.scaleFactor - vlg.preferredHeight;
+
+		outputText.text = s0 + " --- " + s1 + " | " + s2 + " | " + s3 + " )( <b>" + freeHeight + "</b>";
+    }
+}
--- Orthodrome/Assets/Scripts/Scheduler.cs
+++ Orthodrome/Assets/Scripts/Scheduler.cs
@@ -4,18 +4,50 @@ using UnityEngine.UI;
 
 public class Scheduler : MonoBehaviour {
 
-	[Header("Screen Areas")]
-	public GameObject topArea;
-	public GameObject bottomArea;
-	public GameObject leftArea;
-	public GameObject rightArea;
-	public GameObject centerArea;
-	public GameObject frontArea;
-
 	[Header("Prefabs")]
 	public Notification notificationPrefab;
 
+	private Canvas canvas;
+
+	private GameObject topArea;
+	private GameObject bottomArea;
+	private GameObject leftArea;
+	private GameObject rightArea;
+	private GameObject centerArea;
+	private GameObject frontArea;
+
 	//************************************************************
+
+	void Awake() {
+		canvas = FindObjectOfType<Canvas>();
+
+		topArea = canvas.transform.Find("Top Area").gameObject;
+		bottomArea = canvas.transform.Find("Bottom Area").gameObject;
+		leftArea = canvas.transform.Find("Left Area").gameObject;
+		rightArea = canvas.transform.Find("Right Area").gameObject;
+		centerArea = canvas.transform.Find("Center Area").gameObject;
+		frontArea = canvas.transform.Find("Front Area").gameObject;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			// Destroy random notification.
+			System.Random rng = new System.Random();
+			int ln = leftArea.transform.childCount;
+			int n = ln + rightArea.transform.childCount;
+			int i = rng.Next(n);
+
+			GameObject sj;
+
+			if (i < ln)
+				sj = leftArea.transform.GetChild(i).gameObject;
+			else
+				sj = rightArea.transform.GetChild(i - ln).gameObject;
+
+			print(string.Format($"~Destroy~ {ln}+rn={n}, {i} -- object: {sj}"));
+			Destroy(sj);
+		}
+	}
 
 	/// <summary>
 	/// Attempt to display a notification. Return true or false, depending
@@ -25,9 +57,36 @@ public class Scheduler : MonoBehaviour {
 	/// <param name="description">Notification description text</param>
 	/// <returns>Returns true if notification's been displayed succesfully.</returns>
 	public bool Request(string title, string description) {
-		Notification obj = Instantiate(notificationPrefab, leftArea.transform) as Notification;
+
+		// Create a Notification object.
+		Notification obj = Instantiate(notificationPrefab) as Notification;
 		obj.titleText.text = title;
 		obj.descriptionText.text = description;
+
+		// Update Canvas in order to update size of notification's RectTransform.
+		Canvas.ForceUpdateCanvases();
+		var height = obj.GetComponent<RectTransform>().rect.height * canvas.scaleFactor;
+
+		// Assign a parent area.
+		GameObject parent = null;
+		//parent = (Random.Range(0f, 1f) > .5f) ? rightArea.transform : leftArea.transform;
+		parent = leftArea;
+
+		if(parent.GetComponent<RectTransform>().rect.height * canvas.scaleFactor - parent.GetComponent<VerticalLayoutGroup>().preferredHeight < height) {
+			parent = rightArea;
+		}
+
+		obj.transform.SetParent(parent.transform);
+
+		// Update parent parameters.
+		// Canvas.ForceUpdateCanvases();
+		parent.GetComponent<VerticalLayoutGroup>().CalculateLayoutInputVertical();
+
+		// On failure, destroy notification and return false.
+		if (parent == null) {
+			Destroy(obj);
+			return false;
+		}
 
 		return true;
 	}
