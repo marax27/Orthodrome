d4e7db0bfcf1af69b7d00c248f986ceddc7f4ffa
marax27 <kacpert314@gmail.com>
2019-03-23 00:26:15 +0100

    Update CenterArea and ObserverController. Add AdjustOrbitRadius()
    * Coroutine AdjustOrbitRadius makes sure that the planet stays within Center Area's bounds.
    * ObserverController.observerSpeed now refers to linear - not angular - speed.
    * A few minor changes

--- Orthodrome/Assets/Scripts/CenterArea.cs
+++ Orthodrome/Assets/Scripts/CenterArea.cs
@@ -11,9 +11,21 @@ public class CenterArea : Area {
 
 	public float diameterMultiplier = 1.2f;
 
-    void Update() {
+	void Update() {
 		// Signal if the sphere is out of bounds.
 		GetComponent<Image>().color = IsEntireSphereInArea() ? Color.clear : Color.red;
+	}
+
+	/// <summary>
+	/// Get length of a side that surrounds the sphere.
+	/// </summary>
+	float GetSphereSpan() {
+		Vector3 rightDir = Camera.main.transform.right;
+		Vector3 rightPoint = sphere.transform.position + diameterMultiplier * rightDir * sphere.transform.localScale.x;
+
+		Vector2 screenSphereCenter = Camera.main.WorldToScreenPoint(sphere.transform.position);
+		float result = 2 * ((Vector2)Camera.main.WorldToScreenPoint(rightPoint) - screenSphereCenter).magnitude;
+		return result;
 	}
 
 	/// <summary>
@@ -21,12 +33,7 @@ public class CenterArea : Area {
 	/// Returns false if the sphere intersects any of the area's boundaries.
 	/// </summary>
 	public bool IsEntireSphereInArea() {
-		Vector3 rightDir = Camera.main.transform.right;
-		Vector3 rightPoint = sphere.transform.position + diameterMultiplier * rightDir * sphere.transform.localScale.x;
-
-		Vector2 screenSphereCenter = Camera.main.WorldToScreenPoint(sphere.transform.position);
-		float visibleDiameter = 2 * ((Vector2)Camera.main.WorldToScreenPoint(rightPoint) - screenSphereCenter).magnitude;
-
-		return visibleDiameter < GetWidth() && visibleDiameter < GetHeight();
+		float span = GetSphereSpan();
+		return span < GetWidth() && span < GetHeight();
 	}
 }
--- Orthodrome/Assets/Scripts/ObserverController.cs
+++ Orthodrome/Assets/Scripts/ObserverController.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObserverController : MonoBehaviour {
 
+	[Tooltip("Linear (not angular) speed of the observer.")]
 	public float observerSpeed = 10f;
-	public Transform objectOfInterestTransform;
 	public float orbitRadius = 40f;
 
 	public bool devMode = false;
+
+	public Transform objectOfInterestTransform;
+	public CenterArea centerArea;
 
 	Vector3 velocity;
 	float orbitPhase;  //angle that determines viewer's position around the planet
@@ -30,11 +34,32 @@ public class ObserverController : MonoBehaviour {
 					 + transform.right * Input.GetAxisRaw("Horizontal")
 					 + transform.up * verticalAxisMove;
 		} else {
-			orbitPhase += observerSpeed * Time.deltaTime;
+			orbitPhase += observerSpeed / orbitRadius * Time.deltaTime;
+
+			// Check if the planet isn't out of bounds.
+			if (!centerArea.IsEntireSphereInArea())
+				StartCoroutine(AdjustOrbitRadius());
 		}
 	}
 
 	void LateUpdate() {
 		transform.LookAt(objectOfInterestTransform);
+	}
+
+	/// <summary>
+	/// Increase or decrease radius of the observer's orbit so that the planet will occupy
+	/// as much center area's space as possible, but it will be fully within this area.
+	/// </summary>
+	IEnumerator AdjustOrbitRadius() {
+		bool initialState = centerArea.IsEntireSphereInArea();
+		float radiusStep = .05f * (initialState ? -1 : +1);
+
+		while (centerArea.IsEntireSphereInArea() == initialState) {
+			orbitRadius += radiusStep;
+			yield return new WaitForEndOfFrame();
+		}
+
+		if(!centerArea.IsEntireSphereInArea())
+			orbitRadius += 2f * Mathf.Abs(radiusStep);
 	}
 }
