33ee913a288df604ed517a2c5c2fe281a1db562e
marax27 <kacpert314@gmail.com>
2019-03-27 19:15:02 +0100

    Add IPlugin

---
+++ Orthodrome/Assets/Scripts/IPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// The base of any Orthodrome plugin. IPlugin itself offers
+/// little functionality, but every plugin class must implement it.
+/// </summary>
+public interface IPlugin {
+
+	NotificationFactory Factory { set; }
+	Scheduler Scheduler { set; }
+
+	// Properties.
+	string Name { get; }
+	string Author { get; }
+	string Description { get; }
+
+}
