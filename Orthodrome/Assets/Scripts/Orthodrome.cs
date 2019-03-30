92f35cbd5b5c4b9e3273a7da71f12d3ff328ef99
marax27 <kacpert314@gmail.com>
2019-03-30 23:26:48 +0100

    Update orthodrome computation
    1. Handle the case in which orthodrome has to cross the 180th meridian.
    2. Rearrange orthodrome equation. Reduced number of operations to execute per point.

--- Orthodrome/Assets/Scripts/Orthodrome.cs
+++ Orthodrome/Assets/Scripts/Orthodrome.cs
@@ -14,7 +14,18 @@ public class Orthodrome : GeoLine {
 		lng1 = Mathf.Deg2Rad * startPoint.longitude;
 		lat2 = Mathf.Deg2Rad * endPoint.latitude;
 		lng2 = Mathf.Deg2Rad * endPoint.longitude;
-		lng_diff = Mathf.Abs(lng2 - lng1);
+		lng_diff = lng2 - lng1;
+
+		coef1 = Mathf.Tan(lat2) * Mathf.Cos(lng1) - Mathf.Tan(lat1) * Mathf.Cos(lng2);
+		coef2 = Mathf.Tan(lat2) * Mathf.Sin(lng1) - Mathf.Tan(lat1) * Mathf.Sin(lng2);
+
+		// Fixed: wrong curve plotted when an orthodrome crossed the 180th meridian.
+		if (Mathf.Abs(startPoint.longitude - endPoint.longitude) > 180f) {
+			if (startPoint.longitude < 0f)
+				lng1 += 360f * Mathf.Deg2Rad;
+			else
+				lng2 += 360f * Mathf.Deg2Rad;
+		}
 
 		// Compute all points except for the first and last one.
 		for (int i = 1; i < Points.Length - 1; ++i) {
@@ -28,17 +39,16 @@ public class Orthodrome : GeoLine {
 		if (lng_diff == 0f)
 			return new GeoCoord(Mathf.Lerp(lat1, lat2, percent), lng1);
 
-		// TODO: vulnerable to the -180/+180 meridian.
 		float lng = Mathf.Lerp(lng1, lng2, percent);
 
 		// Compute tan(lat).
-		float tan_lat = Mathf.Sin(lng) * (Mathf.Tan(lat2) * Mathf.Cos(lng1) - Mathf.Tan(lat1) * Mathf.Cos(lng2));
-		tan_lat -= Mathf.Cos(lng) * (Mathf.Tan(lat2) * Mathf.Sin(lng1) - Mathf.Tan(lat1) * Mathf.Sin(lng2));
-		tan_lat /= Mathf.Sin(lng2 - lng1);
+		float tan_lat = Mathf.Sin(lng) * coef1 - Mathf.Cos(lng) * coef2;
+		tan_lat /= Mathf.Sin(lng_diff);
 
 		return new GeoCoord(Mathf.Atan(tan_lat) * Mathf.Rad2Deg, lng * Mathf.Rad2Deg);
 	}
 
 	float lat1, lng1, lat2, lng2;
 	float lng_diff;
+	float coef1, coef2;
 }
